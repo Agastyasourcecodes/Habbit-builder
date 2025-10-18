7d2c4da59372a267c91700b98b68dbeed6f3f148
Agastyasourcecodes <agastyamaurya123@gmail.com>
2025-10-18 17:47:57 +0530

    Fix Navbar import casing for Linux build

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import { useEffect, useState } from "react";
 import RateLimitedUI from "../components/RateLimitedUI";
 import axios from "axios";
